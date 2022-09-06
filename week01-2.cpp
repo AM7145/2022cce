569533bd6cb7841c3005172a29a7cde7a2c3385e
AM7145 <112918653+AM7145@users.noreply.github.com>
2022-09-06 11:28:23 +0800

    Add files via upload

---
+++ week01-2.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ int main()
+ {
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("中文有時候會有問題\n");
+
+    ///中文有時候會有問題
+    //Code Blocks畫3條線比較深
+    ///兩條線比較淡
+
+
+
+
+ }
