aaeb0fc91d0477046dbb09d4f8c777b7857e1706
AM7145 <112918653+AM7145@users.noreply.github.com>
2022-09-13 12:02:35 +0800

    week02

---
+++ week02-1.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main()
+{
+    printf("Hello World\n");
+    printf("H\tello World\n");
+    printf("\tHello World\n");
+
+    system("pause");
+    return 0;
+
+}
---
+++ week02-3.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main()
+{
+    int a;
+
+    a=2;
+    printf("現在a是%d\n", a);
+
+    a=99;
+    printf("現在a是%d\n", a);
+
+    system("pause");
+    return 0;
+
+}
+
