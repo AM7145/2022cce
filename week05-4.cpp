6b41d817ea1601dc95a5f952be456c413aec607d
AM7145 <112918653+AM7145@users.noreply.github.com>
2022-10-04 12:01:54 +0800

    week05

---
+++ week05-4.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+
+    for(int a=0;a<3; a++){
+        printf("(基礎型) a=%d\n", a);
+    }
+
+    printf("\n");///看起來好看用的
+
+    for(int a=1;a<=3; a++){
+        printf("(人類型) a=%d\n", a);
+    }
+
+    printf("\n");
+
+    for(int a=3;a>0; a--){
+        printf("(倒數型) a=%d\n", a);
+    }
+
+
+
+
+
+}
