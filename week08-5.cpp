d6856788efe3796fa614b1620b421b810cbdbb02
AM7145 <112918653+AM7145@users.noreply.github.com>
2022-10-25 13:33:01 +0800

    week08-5

--- week08-5.cpp
+++ week08-5.cpp
@@ -1,19 +1,17 @@
 #include<stdio.h>
 int main()
 {
-	int n;
-	scanf("%d", &n);
+    int n;
+    scanf("%d", &n);
 
-	int i=1;
-	while(i<=n){
-		int k=1;
-		while(k<=n){
-			if(k<=n-i) printf(" ");
-			else printf("*");
+    for(int i=1;i<=n;i++){
+        for(int k=1;k<=n;k++){
+            if(k<=n-i) printf(" ");
+            else printf("*");
+        }
 
-			k++;
-		}
-	printf("\n");
-	i++;
-	}
+
+        printf("\n");
+    }
+
 }
